29a86b85b79d470e31a8f80aa3a2d12e7243f61e
YOGESH <j.yogesh8458@gmail.com>
2026-01-14 10:00:29 +0530

    tenth sorting

--- SORTING/Bubblesort.java
+++ SORTING/Bubblesort.java
@@ -1,5 +1,27 @@
 package SORTING;
-
+import java.util.Arrays;
 public class Bubblesort {
+
+  public static void bubbleSort(int[] arr){
+    for ( int i =  0 ; i< arr.length - 1 ; i++){
+      for (int j = 0 ; j < arr.length - 1 -i ; j++){
+        if (arr[j] > arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+      }
+    }
+  }
+  public static void main(String[] args){
+
+    int arr[] = { 12 ,3,112,12,11,12,122};
+
+    bubbleSort(arr);
+
+    System.out.println(Arrays.toString(arr));
+
+
+  }
   
 }
