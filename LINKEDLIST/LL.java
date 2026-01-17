21bb8bf57c42971c76736fe957f0f26b5ba7651e
YOGESH <j.yogesh8458@gmail.com>
2026-01-17 17:44:10 +0530

    Linked  list 15 java

--- LINKEDLIST/LL.java
+++ LINKEDLIST/LL.java
@@ -8,6 +8,33 @@ public class LL{
 
     }
 
+//display
+public void display(){
+  Node temp = head;
+  while(temp != null){
+    System.out.print(  temp.value + "->");
+    temp = temp.next;
+  }
+  System.out.println("END");
+}
+
+
+//Insert at first 
+  public void insertFirst(int val){
+    Node node = new Node(val);
+    node.next = head; 
+    head = node;
+
+    if(tail == null){
+      tail = head;
+    }
+
+    size += 1;
+  }
+
+
+//
+
   private class Node{
 
     private int value;
--- LINKEDLIST/Main.java
+++ LINKEDLIST/Main.java
@@ -1,6 +1,13 @@
+
+
 public class Main {
   public static void main(String[] args) {
-    
+    LL list = new LL();
+    list.insertFirst(3);
+    list.insertFirst(2);
+    list.insertFirst(8);
+    list.insertFirst(15);
+    list.display();
   }
   
 }
