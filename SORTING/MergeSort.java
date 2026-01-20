2bd749679543c3ca0e7be975f84b8f4796e03122
YOGESH <j.yogesh8458@gmail.com>
2026-01-20 22:58:46 +0530

    18 merge sort

---
+++ SORTING/MergeSort.java
@@ -0,0 +1,15 @@
+package SORTING;
+
+public class MergeSort {
+
+  public static void  divide(int arr[] , int si , int ei) //si == 
+
+
+
+  }
+  public static void main(String[] args) {
+    int arr[] = { 6 ,3 , 9, 5, 2, 8};
+    int n = arr.length;
+  }
+  
+}
