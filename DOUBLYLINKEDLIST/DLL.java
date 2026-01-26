c9d3bab68629429a280e2a735bc867ec30f80a0f
YOGESH <j.yogesh8458@gmail.com>
2026-01-26 14:36:46 +0530

    Doubly linked list 19

---
+++ DOUBLYLINKEDLIST/DLL.java
@@ -0,0 +1,55 @@
+package DOUBLYLINKEDLIST;
+
+public class DLL {
+
+  Node head ; 
+
+
+
+  public void insertFirst(int val){
+
+    Node node = new Node(val);
+
+    node.next = head;
+    node.prev = null;
+    if(head != null){
+      head.prev = node;
+    }
+    head = node;
+    
+
+
+  }
+
+
+  public void display(){
+    Node node = head ; //we cant move head because it will alter the value of the dll 
+    while(node != null){
+      System.out.println(node.val + " -> ");
+      node =node.next;
+    }
+
+    System.out.println();
+  }
+
+
+
+
+  private class Node{
+    int val;
+    Node next;
+    Node prev;
+
+    public Node(int val){
+      this.val = val;
+    }
+
+    public Node(int val , Node next , Node prev){
+      this.val = val;
+      this.next = next ;
+      this.prev = prev;
+    }
+
+  }
+  
+}
---
+++ DOUBLYLINKEDLIST/Main.java
@@ -0,0 +1,10 @@
+package DOUBLYLINKEDLIST;
+
+public class Main {
+  public static void main(String[] args) {
+    DLL list = new DLL();
+    list.insertFirst(2);
+     
+  }
+  
+}
